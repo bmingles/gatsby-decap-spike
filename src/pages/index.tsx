46fab483eb9396ae2d2e3b7f47a7583fdfa41e43
Brian Ingles <github@emeraldwalk.com>
2023-09-01 22:55:58 -0500

    Adding decap cms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,5 +15,16 @@ export const Head: HeadFC = () => (
   <>
     <Seo title="Home Page" />
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    <script>
+      {`if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }`}
+    </script>
   </>
 )
